b034ff052c5f99ebe0ad67c6c712378da123f533
Hirok Sengupta <senguptahirok@gmail.com>
2024-11-22 11:59:28 +0530

    added body-parser package for POST

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-/* const bodyParser = require('body-parser'); 
-const m_connect = require('mongoose');
+const bodyParser = require('body-parser'); 
+/* const m_connect = require('mongoose');
 require('dotenv').config() */
 
 app.use(cors())
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 /*
 let m_uri = process.env.MONGO_URI;
 m_connect.connect(m_uri,{useNewUrlParser: true, useUnifiedTopology: true});
@@ -26,9 +26,9 @@ let userSchema = new m_connect.Schema({
 User = m_connect.model('User',userSchema);
 */
 /* POST /api/users */
-/* app.post('/api/users',function(req,res){
+ app.post('/api/users',function(req,res){
   console.log('req.body = ' + req.body);
-  let createAndSaveUser = function(done){
+/*  let createAndSaveUser = function(done){
     let a = req.body;
     let b = new User(a);
     b.save(function(err,data){
@@ -36,10 +36,10 @@ User = m_connect.model('User',userSchema);
       else console.log('data = ' + data);
     });
     done(null);
-  }; 
+  }; */
   res.send({'user info': req.body});
 });
-*/
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
