076bb78f097143ea66a94b1dabe5dd1fc3c49d59
Hirok Sengupta <senguptahirok@gmail.com>
2024-11-22 17:29:26 +0530

    including back the mongoose portion

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser'); 
-/* const m_connect = require('mongoose');
-require('dotenv').config() */
+const m_connect = require('mongoose');
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.static('public'))
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(bodyParser.urlencoded({extended: false}));
-/*
+
 let m_uri = process.env.MONGO_URI;
 m_connect.connect(m_uri,{useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -24,20 +24,34 @@ let userSchema = new m_connect.Schema({
   }
 });
 User = m_connect.model('User',userSchema);
-*/
+
 /* POST /api/users */
  app.post('/api/users',function(req,res){
-  console.log('req.body = ' + Object.values(req.body));
-/*  let createAndSaveUser = function(done){
-    let a = req.body;
+  let userNameStr = Object.values(req.body).toString();
+  console.log('req.body = ' + userNameStr);
+  /* create a new User entry in User Schema of MongoDB */
+  let createAndSaveUser = function(done){
+    let a = userNameStr;
     let b = new User(a);
     b.save(function(err,data){
       if (err) console.log ('error = ' + err);
       else console.log('data = ' + data);
     });
     done(null);
-  }; */
-  res.send({'user info': Object.values(req.body).toString()});
+  };
+
+  /* find an User by name from the MongoDB database */
+  let userN = userNameStr;
+  let findUser = function(userN, done){
+    User.find({username: userN},function(err,userFound){
+      if (err) console.log('error in finding the user in User model in MongoDB');
+      else {
+        console.log('user was found = ' + userFound);
+        res.json({'username': username,'_id': _id}); 
+      }
+    });
+    done(null,data);
+  };
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
