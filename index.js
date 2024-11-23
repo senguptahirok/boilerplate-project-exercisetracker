3ac553235aa732cace78d4ca2e1a2d06ca620ee5
Hirok Sengupta <senguptahirok@gmail.com>
2024-11-23 13:38:50 +0530

    create new user

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser'); 
-const m_connect = require('mongoose');
-require('dotenv').config();
+/* require('dotenv').config(); */
 
 app.use(cors())
 app.use(express.static('public'))
@@ -13,49 +12,22 @@ app.get('/', (req, res) => {
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-let m_uri = process.env.MONGO_URI;
-m_connect.connect(m_uri,{useNewUrlParser: true, useUnifiedTopology: true});
-
-let User;
-let userSchema = new m_connect.Schema({
-  username:{
-    type: String,
-    required: true
-  }
-});
-User = m_connect.model('User',userSchema);
-exports.UserModel = User;
-
 /* POST /api/users */
  app.post('/api/users',function(req,res){
   let userNameStr = Object.values(req.body).toString();
   console.log('req.body = ' + userNameStr);
-  /* create a new User entry in User Schema of MongoDB */
-  let createAndSaveUser = function(done){
-    let a = userNameStr;
-    let b = new User(a);
-    b.save(function(err,data){
-      if (err) console.log ('error = ' + err);
-      else console.log('data = ' + data);
-      done(null,data);
-    });
-//    done(null);
-  };
-  exports.createAndSaveUser = createAndSaveUser;
-
-  /* find an User by name from the MongoDB database */
-  /* let userN = userNameStr;
-  let findUser = function(userN, done){
-    User.find({username: userN},function(err,userFound){
-      if (err) console.log('error in finding the user in User model in MongoDB');
-      else {
-        console.log('user was found = ' + userFound);
-        res.json({'username': username,'_id': _id}); 
-      }
-    });
-    done(null,data);
-  };*/
-  res.json({'username': userNameStr});
+  if (!req.body) res.json({success: false, msg: 'please pass user name'});
+  else {
+    let newUser = new User({
+      name: userNameStr
+    })
+  }
+  /* save the user */
+  newUser.save(function(err, data){
+    if (err) res.json({success: false, msg: 'Username already exists'});
+    else res.json({'username': userNameStr,'_id': _id});
+  });
+  /* res.json({'username': userNameStr,'_id': _id}); */
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
