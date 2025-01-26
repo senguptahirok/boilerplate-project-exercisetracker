eee494c16fb7ad2c6a8b2c8ec26acbda89c9c753
Hirok Sengupta <senguptahirok@gmail.com>
2025-01-27 01:41:33 +0530

    trying to add a new user in mongodb

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser');
 require('dotenv').config()
+let m_uri = process.env.MONGO_URI;
+
+let User;
+let m_connect = require('mongoose');
+m_connect.connect(m_uri,{useNewUrlParser: true, useUnifiedTopology: true});
+
+let userSchema = new m_connect.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+});
+
+User = m_connect.model('User',userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -15,6 +29,11 @@ let host01 = '';
 app.post('/api/users',function(req,res){
   host01 = Object.values(req.body);
   host01 = host01.toString();
+  let a = {name: host01};
+  let b = new User(a);
+  b.save(function(err,data){
+    if (err) console.log('error = '+ err);
+  });
   res.json({"host": host01});
 });
 
