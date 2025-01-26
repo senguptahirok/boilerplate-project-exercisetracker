2245f4be14965509dc47664f10acf0845406d209
Hirok Sengupta <senguptahirok@gmail.com>
2025-01-27 00:52:51 +0530

    app.post testing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 app.use(cors())
@@ -9,9 +10,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.use(bodyParser.urlencoded({extended: false}));
+let host01 = '';
+app.post('/api/users',function(req,res){
+  host01 = Object.values(req.body);
+  host01 = host01.toString();
+  res.json({"host": host01});
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
