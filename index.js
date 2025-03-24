05323fdba7d37ec8a70f11ee71d0d18dcde4558e
Hirok Sengupta <senguptahirok@gmail.com>
2025-03-24 19:49:45 +0530

    debugging

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app.post('/api/users',function(req,res){
 let host02 = [];
 app.post('/api/users/:_id/exercises', function(req,res){
   host01 = Object.values(req.body);
+  host01 = host01.toString();
   for (value in host01) {host02.push(value)};
   console.log(' **** in exercise area **** ');
   console.log('host01 = ' + host01);
