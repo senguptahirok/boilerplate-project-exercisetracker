55de9970760ab18775f2339ae08d986d4376f005
Hirok Sengupta <senguptahirok@gmail.com>
2025-03-24 17:58:32 +0530

    debugging the exercise part

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/api/users',function(req,res){
 let host02 = [];
 app.post('/api/users/:_id/exercises', function(req,res){
   host01 = Object.values(req.body);
-  for (value in host01) {host02 = host02.push(value)};
+  for (value in host01) {host02.push(value)};
   console.log(' **** in exercise area **** ');
   console.log('host01 = ' + host01);
   console.log('type of host01 = ' + typeof(host01));
