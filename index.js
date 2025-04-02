a62498084081d1910a93a81f64bfc891f178db80
Hirok Sengupta <senguptahirok@gmail.com>
2025-04-02 17:32:01 +0530

    debugging

--- index.js
+++ index.js
@@ -81,15 +81,14 @@ app.post('/api/users/:_id/exercises', function(req,res){
 
     /* get username from the User Schema, based on the _id */
     let userId = data.id;
-    console.log('userId = ' + userId);
     let userN = '';
-    User.findById({_id: userId, function(err01,data01){
+    User.findById({_id: userId}, function(err01,data01){
       console.log(' ***** in function findById ****** ');
       if (err01) console.log('user id = ' + userId + 'is not present in User Schema, error = ' + err01);
       else console.log('data01 = ' + data01);
       userN = data01.name;
       console.log('userN = ' + userN);         
-    }});
+    });
     b_exercise = {"username": userN, "description": data.description, 
                   "duration": data.duration, "date": data.date,
                   "_id": data.id};
