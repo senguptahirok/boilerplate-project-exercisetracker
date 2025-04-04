afedf789539ea449423b0fbc63c4133cce402985
Hirok Sengupta <senguptahirok@gmail.com>
2025-04-04 19:40:15 +0530

    debugging the date part

--- index.js
+++ index.js
@@ -68,6 +68,8 @@ app.post('/api/users/:_id/exercises', function(req,res){
   let a = {id: host03[0], description: host03[1], duration: host03[2], date: host03[3]};
 
   /* to load the current date, if the date field is not filled-in */
+  console.log('host03[3] = ' + host03[3]);
+  console.log('type of host03[3] = ' + typeof(host03[3]));
   if (host03[3] === null) 
     host03[3] = new Date().toISOString().substring(0,10);
 
