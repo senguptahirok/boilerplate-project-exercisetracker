0162a82238db088352154504ee31e6aeaeac76dd
Hirok Sengupta <senguptahirok@gmail.com>
2025-05-19 17:25:25 +0530

    debugging

--- index.js
+++ index.js
@@ -112,13 +112,14 @@ app.get('/api/users', function(req,res){
 });
 
 /* get username from the User Schema, based on the _id */
-let findUserById = function(userId){
+const findUserById = function(userId, done){
     console.log('inside function findUserByid, userId = ' + userId);
     User.findById({_id: userId}, function(err01,data01){
       if (err01) console.log('user id = ' + userId + 'is not present in User Schema, error = ' + err01);
       else {
-        console.log('data01 = ' + data01)
-        return (data01)
+        console.log('data01 = ' + data01);
+        // return (data01)
+        done(null, data01);
       };
     });
 };
