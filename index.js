bfa777e5daac785c51ba6c4b8cfa53671854fce7
Hirok Sengupta <senguptahirok@gmail.com>
2025-05-26 12:23:27 +0530

    debugging

--- index.js
+++ index.js
@@ -112,8 +112,8 @@ app.get('/api/users', function(req,res){
 /* get a full exercise log of any user */
 app.get('/api/users/:_id/logs', function(req,res){
   console.log('in api/users/:_id/logs ' + req.params._id);
-  User.findById({id: req.params._id}, function(err, data){
-    console.log('req.params._id = ' + req.params._id);
+  User.findOne({id: req.params._id}, function(err, data){
+    console.log('req.params._id = ' + id);
     if (err) console.log('user id = ' + _id + 'does not have any exercises');
     else console.log('data exercise log = ' + data);
     res.json(data);
