87cd32733ddac35d07eef82780d033e3025ea133
Hirok Sengupta <senguptahirok@gmail.com>
2025-07-06 09:14:43 +0530

    displaying data content

--- index.js
+++ index.js
@@ -140,7 +140,8 @@ app.get('/api/users/:_id/logs', function(req,res){
     }
 /*    res.json({"username": logObj_name, "count": logObj_count, "_id": logObj_id, 
               "log": {"description": logObj_desc, "duration": logObj_dur, "date": data.date}}); */
-    res.send(data.description);
+      console.log('data = ' + data);
+      res.send(data);
   });
 });
 
