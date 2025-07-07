4fc847318a63259f7787e41cd7a7ba2b8bad12da
Hirok Sengupta <senguptahirok@gmail.com>
2025-07-07 08:53:33 +0530

    debugging

--- index.js
+++ index.js
@@ -142,8 +142,8 @@ app.get('/api/users/:_id/logs', function(req,res){
 /*    res.json({"username": logObj_name, "count": logObj_count, "_id": logObj_id, 
               "log": {"description": logObj_desc, "duration": logObj_dur, "date": data.date}}); */
       
-      console.log('data = ' + data);
-      res.json(data);
+      console.log('partial data = ' + {description, duration, date});
+      res.json({description, duration, date});
   });
 });
 
