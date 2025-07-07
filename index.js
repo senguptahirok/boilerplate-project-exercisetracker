c2bde97a0d0f1d79b39d968f6641985031d503c0
Hirok Sengupta <senguptahirok@gmail.com>
2025-07-07 08:42:24 +0530

    debugging the data part

--- index.js
+++ index.js
@@ -128,6 +128,7 @@ app.get('/api/users/:_id/logs', function(req,res){
 
   /* get the respective user's exercise log and count */
   Exercise.find({id: req.params._id}, function(err, data){
+    const {description,duration,date} = data;
     if (err) console.log('user id = ' + req.params._id + 'does not have any exercises');
     else {
       logObj_desc = data.description;
@@ -141,13 +142,8 @@ app.get('/api/users/:_id/logs', function(req,res){
 /*    res.json({"username": logObj_name, "count": logObj_count, "_id": logObj_id, 
               "log": {"description": logObj_desc, "duration": logObj_dur, "date": data.date}}); */
       
-      let data01 = data.map(function(a){
-         a.description,
-         a.duration,
-         a.date
-      });
-      console.log('data01 = ' + data01);
-      res.send(data01);
+      console.log('data = ' + data);
+      res.json(data);
   });
 });
 
