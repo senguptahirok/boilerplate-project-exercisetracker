f5be594319ac0b0fe7794976305de81230e9cb56
Hirok Sengupta <senguptahirok@gmail.com>
2025-08-30 23:22:21 +0530

    updating log with limits

--- index.js
+++ index.js
@@ -109,11 +109,20 @@ app.get('/api/users', function(req,res){
   });
 });
 
-/* get a full exercise log of any user */
-app.get('/api/users/:_id/logs', function(req,res){
+/* get a full exercise log of any user, that can also be filtered based on date range and limit of exercises */
+// app.get('/api/users/:_id/logs', function(req,res){
+app.get('/api/users/:_id/logs?:dt_from&:dt_to&:limit', function(req,res){
   let logObj_name = '';
   let logObj_id = '';
   let logObj_count = 0;
+
+  let fromDt = req.params.dt_from;
+  let toDt = req.params.dt_to;
+  let limit = req.params.limit;
+
+  console.log('from date = ' + fromDt);
+  console.log('to date = ' + toDt);
+  console.log('limit = ' + limit);
 
 /* get username from the User Schema, based on the _id */
   User.findById({_id: req.params._id}, function(err01, data01){
